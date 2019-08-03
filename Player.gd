78f3152491beef0bdf3a648631f7de892b4f4064
RobertKajnak <hezirisz@gmail.com>
2019-08-03 11:38:07 +0200

    Set project scaling. Player can now move sideways

--- Player.gd
+++ Player.gd
@@ -8,16 +8,13 @@ func _physics_process(delta):
 	
 	if Input.is_action_pressed("ui_down"):
 		motion.y = 100
-	elif Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("ui_up"):
 		motion.y = -100
-	elif Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("ui_left"):
 		motion.x = -100
-	elif Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
-	else:
-		motion.y = 0
-		motion.x = 0	
+	
 		
-		
-	move_and_slide(motion)
+	motion = move_and_slide(motion)
 	pass
