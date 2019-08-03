562d8ab029b0e3df636392e78cd8bb701ce740ba
KovaxG <georgesmth202@gmail.com>
2019-08-03 18:00:24 +0300

    Added enemy follow AI

--- Enemy.gd
+++ Enemy.gd
@@ -1,10 +1,30 @@
 extends KinematicBody2D
 
 var motion = Vector2()
+var target = null
+var speed = 100
 
 func _physics_process(delta):
-	motion.x = rand_range(-100,100)
-	motion.y = rand_range(-100,100)
+	motion.x = 0
+	motion.y = 0
 	
+	if target == null:
+		look_for_target()
+	else:
+		move_towards_target()
+		
 	motion = move_and_slide(motion)
 	pass
+	
+func move_towards_target():
+	var self_position = position
+	look_at(target.position)
+	rotate(3*PI/2) 
+	motion.x = target.position.x - self_position.x
+	motion.y = target.position.y - self_position.y
+	
+func look_for_target():
+	if $RayCast2D.is_colliding():
+		var object = $RayCast2D.get_collider()
+		if object.name == "Player":
+			target = object
--- Player.gd
+++ Player.gd
@@ -4,6 +4,20 @@ var motion = Vector2()
 var hp = 1.0
 
 func _physics_process(delta):
+
+	rotate_to_mouse()
+	handle_motion()
+	
+	get_parent().find_node("LabelHP").text = 'HP: '+ str(hp)
+	
+func rotate_to_mouse():
+	var mouse_poz = get_global_mouse_position()
+	look_at(mouse_poz)
+	
+	# This is needed for some reason because the player is not facing the mouse otherwise
+	rotate(3*PI/2) 
+	
+func handle_motion():
 	motion.y = 0
 	motion.x = 0
 	
@@ -17,8 +31,4 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		motion.x = 100
 	
-		
 	motion = move_and_slide(motion)
-	
-	get_parent().find_node("LabelHP").text = 'HP: '+ str(hp)
-	#print(labelHP)
