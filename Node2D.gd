2f932ce52994e590612ece58be023d4883d78b3d
RobertKajnak <hezirisz@gmail.com>
2019-08-04 16:44:43 +0200

    Added landing page

--- Node2D.gd
+++ Node2D.gd
@@ -1,15 +1,28 @@
 extends Node2D
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 
-# Called when the node enters the scene tree for the first time.
+
+export(String) var contents
+export(String,FILE,'*.txt') var contents_from_file
+export(String,FILE,'*.tscn') var next_scene
+
+var mouse_debounced = 0
+
+func read_all_lines(filename):
+    var file = File.new()
+    file.open(filename, File.READ)
+    var content = file.get_as_text()
+    file.close()
+    return content
+
 func _ready():
-	var armor = preload("res://Armor.tscn").instance()
-	armor.position = Vector2(100,100)
-	get_parent().add_child(armor)
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	if contents == null or len(contents)<2:
+		$RichTextContents.text = read_all_lines(contents_from_file)
+	else:
+		$RichTextContents.text = contents
+	
+func _process(delta):
+	if Input.is_mouse_button_pressed(BUTTON_LEFT):
+		if mouse_debounced:
+			get_tree().change_scene(next_scene)
+	else:
+		mouse_debounced = true
