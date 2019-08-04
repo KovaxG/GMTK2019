665ff0b8345eb10f51ab779e5e16f0ad39853a2b
KovaxG <georgesmth202@gmail.com>
2019-08-04 20:16:02 +0300

    Added start screens

--- Node2D.gd
+++ Node2D.gd
@@ -2,23 +2,12 @@ extends Node2D
 
 
 export(String) var contents
-export(String,FILE,'*.txt') var contents_from_file
 export(String,FILE,'*.tscn') var next_scene
 
 var mouse_debounced = 0
 
-func read_all_lines(filename):
-    var file = File.new()
-    file.open(filename, File.READ)
-    var content = file.get_as_text()
-    file.close()
-    return content
-
 func _ready():
-	if contents == null or len(contents)<2:
-		$RichTextContents.text = read_all_lines(contents_from_file)
-	else:
-		$RichTextContents.text = contents
+	pass
 	
 func _process(delta):
 	if Input.is_mouse_button_pressed(BUTTON_LEFT):
